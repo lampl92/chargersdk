dcfbf21dfd7976ed6c2416c4a336395500758ae2
rgw <rgw5267@gmail.com>
2016-12-02 13:18:14 +0800

    去掉ezxml

--- src/cli/cli_xml.c
+++ src/cli/cli_xml.c
@@ -1,5 +1,4 @@
 #include "includes.h"
-#include "ezxml.h"
 #include "mxml.h"
 
 static void cli_testxml_create_fnt(int argc, char **argv)
@@ -83,19 +82,7 @@ static void cli_testxml_create_fnt(int argc, char **argv)
 
 static void cli_testxml_fnt(int argc, char **argv)
 {
-    ezxml_t f1 = ezxml_parse_file("formula1.xml"), team, driver;
-    const char *teamname;
 
-    for (team = ezxml_child(f1, "team"); team; team = team->next)
-    {
-        teamname = ezxml_attr(team, "name");
-        for (driver = ezxml_child(team, "driver"); driver; driver = driver->next)
-        {
-            printf("%s, %s: %s\n", ezxml_child(driver, "name")->txt, teamname,
-                   ezxml_child(driver, "points")->txt);
-        }
-    }
-    ezxml_free(f1);
 }
 
 tinysh_cmd_t cli_testxml_cmd = {0, "testxml", "xmlΩ‚Œˆ≤‚ ‘", "[args]",
