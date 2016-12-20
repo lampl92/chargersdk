b836fca3461a7f7f14466da45ea9555e699acf16
rgw <rgw5267@gmail.com>
2016-12-20 16:54:13 +0800

    软件版本

---
+++ build/EmBitz/version.h
@@ -0,0 +1,30 @@
+#ifndef VERSION_H
+#define VERSION_H
+
+	//Date Version Types
+	#define DATE "20"
+	#define MONTH "12"
+	#define YEAR "2016"
+	#define UBUNTU_VERSION_STYLE "16.12"
+	
+	//Software Status
+	#define STATUS "Alpha"
+	#define STATUS_SHORT "a"
+	
+	//Standard Version Type
+	#define MAJOR 1
+	#define MINOR 0
+	#define BUILD 11
+	#define REVISION 57
+	
+	//Miscellaneous Version Types
+	#define BUILDS_COUNT 29
+	#define RC_FILEVERSION 1,0,11,57
+	#define RC_FILEVERSION_STRING "1, 0, 11, 57\0"
+	#define FULLVERSION_STRING "1.0.11.57"
+	
+	//These values are to keep track of your versioning state, don't modify them.
+	#define BUILD_HISTORY 11
+	
+
+#endif //VERSION_H
