853ce00f27f3af14eed0abd47dd6ba87dc00cf2d
rgw <rgw5267@gmail.com>
2016-12-20 16:59:55 +0800

    添加注释

--- src/drivers/device/inc/RFIDReader_mt626.h
+++ src/drivers/device/inc/RFIDReader_mt626.h
@@ -1,17 +1,25 @@
 #ifndef __RFIDREADER_MT626_H
 #define __RFIDREADER_MT626_H
-
+/**
+* @file RFIDReader_mt626.h
+* @brief MT626 com driver
+* @author rgw
+* @version v1.0
+* @date 2016-12-14
+*/
 #include "stm32f4xx.h"
 
+/* 命令相关变量 */
 #define MT626_CMD_MAX                   10  //命令数量
 #define MT626_SENDBUFF_MAX              100 //发送缓冲长度
 #define MT626_RECVBUFF_MAX              100 //接收缓冲长度
 
-
+/* 命令帧元素 */
 #define MT626_CMD_STX                   0x02
 #define MT626_CMD_ETX                   0x03
 #define MT626_CMD_TYPE                  0x34
 
+/* 命令号 */
 #define MT626_FIND_CMD                  0   //#0  寻卡
 #define MT626_READ_UID_CMD              1   //#1  获取UID
 #define MT626_AUTH_KEYA_CMD             2   //#3  验证KeyA
@@ -23,14 +31,14 @@
 #define MT626_DEC_CMD                   8   //#9  减值
 #define MT626_INIT_CMD                  9   //#10 初始化
 
-
+/* 命令返回状态 */
 #define MT_STATE_Y                      0x59    //'Y' 成功
 #define MT_STATE_N                      0x4E    //'N' 寻卡失败/获取UID失败
 #define MT_STATE_AUTH_ERR               0x33    //'3' 验证密码错误
 #define MT_STATE_SECTOR_ERR             0x31    //'1' 操作扇区号错误(不是验证密码后的扇区)
 #define MT_STATE_DATA_ERR               0x34    //'4' 读/写数据失败,更改密码失败,增/减/初始化失败
 
-
+/* MT626状态 */
 typedef enum
 {
     MT_SUCCEED,
