45b5b26e01d8ba441600c1d242e67dbb43b84bae
rgw <rgw5267@gmail.com>
2017-02-15 10:04:45 +0800

    增加evse状态获取定时器

--- src/app/timercallback.c
+++ src/app/timercallback.c
@@ -1,13 +1,17 @@
 /**
 * @file timercallback.c
-* @brief
+* @brief 定时器回调函数
 * @author rgw
 * @version v1.0
 * @date 2017-02-13
 */
 #include "includes.h"
 
-void vTimerCallback(TimerHandle_t xTimer)
+
+/** 定时器在taskcreate中创建
+ */
+
+void vChargePointTimerCB(TimerHandle_t xTimer)
 {
     uint32_t ulTotalPoint = pListChargePoint->Total;
     ChargePoint_t *pPoint[ulTotalPoint];
@@ -17,15 +21,19 @@ void vTimerCallback(TimerHandle_t xTimer)
     {
         pPoint[i] =  (ChargePoint_t *)(pListChargePoint->pListPointArray[i]);
     }
-    uxTimerID = pvTimerGetTimerID(xTimer);
+    uxTimerID = (uint32_t)pvTimerGetTimerID(xTimer);
     if(uxTimerID == defTIMERID_Temp)
     {
         for(i = 0; i < ulTotalPoint; i++)
         {
-            pPoint[i]->status.dACLTemp = GetACLTemp(pPoint[i]);
-            pPoint[i]->status.dACNTemp = GetACNTemp(pPoint[i]);
-            pPoint[i]->status.dBTypeConnectorTemp1 = GetBTypeConnectorTemp1(pPoint[i]);
-            pPoint[i]->status.dBTypeConnectorTemp2 = GetBTypeConnectorTemp2(pPoint[i]);
+            pPoint[i]->state.dACLTemp = GetACLTemp(pPoint[i]);
+            pPoint[i]->state.dACNTemp = GetACNTemp(pPoint[i]);
+            if(pPoint[i]->info.ucConnectorType == defConnectorTypeB)
+            {
+                pPoint[i]->state.dBTypeConnectorTemp1 = GetBTypeConnectorTemp1(pPoint[i]);
+                pPoint[i]->state.dBTypeConnectorTemp2 = GetBTypeConnectorTemp2(pPoint[i]);
+            }
+
 //            printf_safe("num = %d, ulIntervalOfGetTemp\n", i);
         }
     }
@@ -33,7 +41,7 @@ void vTimerCallback(TimerHandle_t xTimer)
     {
         for(i = 0; i < ulTotalPoint; i++)
         {
-            pPoint[i]->status.BTypeConnectorLockState = GetBTypeConnectorLock(pPoint[i]);
+            pPoint[i]->state.BTypeConnectorLockState = GetBTypeConnectorLock(pPoint[i]);
 //            printf_safe("num = %d, ulIntervalOfGetLock\n", i);
         }
     }
@@ -41,8 +49,8 @@ void vTimerCallback(TimerHandle_t xTimer)
     {
         for(i = 0; i < ulTotalPoint; i++)
         {
-            pPoint[i]->status.ulCCState = GetCCState(pPoint[i]);
-            pPoint[i]->status.ulCPState = GetCPState(pPoint[i]);
+            pPoint[i]->state.ulCCState = GetCCState(pPoint[i]);
+            pPoint[i]->state.ulCPState = GetCPState(pPoint[i]);
 //            printf_safe("num = %d, defTIMERID_CPCCState\n", i);
         }
     }
@@ -50,14 +58,26 @@ void vTimerCallback(TimerHandle_t xTimer)
     {
         for(i = 0; i < ulTotalPoint; i++)
         {
-            pPoint[i]->status.dChargingVoltage = GetChargingVoltage(pPoint[i]);
-            pPoint[i]->status.dChargingCurrent = GetChargingCurrent(pPoint[i]);
-            pPoint[i]->status.dChargingFrequence = GetChargingFrequence(pPoint[i]);
+            pPoint[i]->state.dChargingVoltage = GetChargingVoltage(pPoint[i]);
+            pPoint[i]->state.dChargingCurrent = GetChargingCurrent(pPoint[i]);
+            pPoint[i]->state.dChargingFrequence = GetChargingFrequence(pPoint[i]);
         }
-    }
-    if(uxTimerID == defTIMERID_EVSEState)
-    {
-
     }
 
 }
+void vEVSETimerCB(TimerHandle_t xTimer)
+{
+    uint32_t uxTimerID;
+
+    uxTimerID = (uint32_t)pvTimerGetTimerID(xTimer);
+
+    if(uxTimerID == defTIMERID_EVSEState)
+    {
+        pEVSE->state.ulScramState = GetScramState();
+        pEVSE->state.ulPEState = GetPEState();
+        pEVSE->state.ulKnockState = GetKnockState();
+        pEVSE->state.ulArresterState = GetArresterState();
+        pEVSE->state.ulPowerOffState = GetPowerOffState();
+    }
+}
+
