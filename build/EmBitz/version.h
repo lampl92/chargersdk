2483f2504b4fc7098a449f796547636a02465a33
rgw <rgw5267@gmail.com>
2017-04-14 15:19:38 +0800

    version increase

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 2
 	#define MINOR 1
-	#define BUILD 1242
-	#define REVISION 6815
+	#define BUILD 1254
+	#define REVISION 6879
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 2120
-	#define RC_FILEVERSION 2,1,1242,6815
-	#define RC_FILEVERSION_STRING "2, 1, 1242, 6815\0"
-	#define FULLVERSION_STRING "2.1.1242.6815"
+	#define BUILDS_COUNT 2145
+	#define RC_FILEVERSION 2,1,1254,6879
+	#define RC_FILEVERSION_STRING "2, 1, 1254, 6879\0"
+	#define FULLVERSION_STRING "2.1.1254.6879"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 42
+	#define BUILD_HISTORY 54
 	
 
 #endif //VERSION_H
