78861f15329a64883f818aad77afd7edea839d6e
rgw <rgw5267@gmail.com>
2017-04-19 18:12:08 +0800

    解决打印不全问题

--- src/cli/cli_rtos.c
+++ src/cli/cli_rtos.c
@@ -9,16 +9,22 @@ void cli_tasklist_fnt(int argc, char **argv)
     uint32_t memused;
     double usedkb;
     uint8_t paddr[20];
+    int i;
 
     printf_safe("\r\n");
     vTaskList((char *)&acTaskStatusBuffer);
     printf_safe("=================================================\r\n");
     printf_safe("任务名      任务状态 优先级   剩余栈 任务序号\r\n");
-    printf_safe("%s\r\n", acTaskStatusBuffer);
+    for(i = 0; i < strlen(acTaskStatusBuffer); i++)
+    {
+        printf_safe("%c", acTaskStatusBuffer[i]);
+    }
     printf_safe("\r\n任务名       运行计数         使用率\r\n");
     vTaskGetRunTimeStats((char *)&acTaskStatusBuffer);
-    printf_safe("%s\r\n", acTaskStatusBuffer);
-
+    for(i = 0; i < strlen(acTaskStatusBuffer); i++)
+    {
+        printf_safe("%c", acTaskStatusBuffer[i]);
+    }
     FreeBytesRemaining = xPortGetFreeHeapSize();
     memused = (configTOTAL_HEAP_SIZE - FreeBytesRemaining) * 100 / configTOTAL_HEAP_SIZE;
     xsprintf((char *)paddr, "%d.%01d%%", memused / 10, memused % 10);
