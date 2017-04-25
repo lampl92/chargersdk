3ca50f075ce3ff85ee88e5a5b5c244d609982487
rgw <rgw5267@gmail.com>
2017-04-25 21:03:46 +0800

    e充网接口

---
+++ src/app/libEcharge/ech_transportlayer.c
@@ -0,0 +1,26 @@
+/**
+* @file ech_transportlayer.c
+* @brief
+* @author rgw
+* @version v1.0
+* @date 2017-02-21
+*/
+#include "libEcharge/ech_err.h"
+ech_ErrorCode_t OpenRemote()
+{
+
+}
+
+ps_ErrorCode_t CloseRemote()
+{
+
+}
+
+ps_ErrorCode_t RecvRemote()
+{
+
+}
+ps_ErrorCode_t SendRemote()
+{
+
+}
