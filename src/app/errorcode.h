f1486739efa9390831b3e26b00fa8a203b9297d7
rgw <rgw5267@gmail.com>
2017-04-25 21:03:21 +0800

    注释格式化

--- src/app/errorcode.h
+++ src/app/errorcode.h
@@ -8,7 +8,7 @@
 #ifndef  __ERRORCODE_H
 #define  __ERRORCODE_H
 
-#include "ff.h"
+#include	"ff.h"
 
 #define THROW_ERROR(_dev, _errcode,_errlevel,_msg)   do{                                           \
                                                     ErrorCode_t _macro_errcode = _errcode;         \
@@ -19,12 +19,12 @@
                                                 }while(0);
 typedef enum _ErrorCode
 {
-    ERR_NO,                 //No Error
-    ERR_SCRAM_FAULT,        //急停发生故障
-    ERR_GSENSOR_FAULT,      //重力加速度传感器发生故障
-    ERR_PE_FAULT,           //保护接地连续性传感器发生故障
-    ERR_POWEROFF_DECT_FAULT,//掉电状态检测器发生故障
-    ERR_ARRESTER_FAULT,     //避雷器发生故障
+    ERR_NO,			//No Error
+    ERR_SCRAM_FAULT,		//急停发生故障
+    ERR_GSENSOR_FAULT,		//重力加速度传感器发生故障
+    ERR_PE_FAULT,		//保护接地连续性传感器发生故障
+    ERR_POWEROFF_DECT_FAULT,	//掉电状态检测器发生故障
+    ERR_ARRESTER_FAULT,		//避雷器发生故障
 
     ERR_CON_METER_FAULT,        //电表通信故障
     ERR_CON_CP_FAULT,           //检测CP故障
@@ -40,28 +40,28 @@ typedef enum _ErrorCode
     ERR_CON_STOPCHARGE,         //结束充电错误
     ERR_RELAY_PASTE,            //  "充电点 触点粘连",
 
-    ERR_FILE_RW,            //文件读写错误
-    ERR_FILE_NO,            //无目标文件
-    ERR_FILE_PARSE,         //"参数解析错误",
-    ERR_FILE_PARAM,         //参数内容错误
-    ERR_SET_PARAM,          //设置参数范围错误
-    ERR_SET_SERIALIZATION,  //JSON序列化错误
-    ERR_OTHER,              //其他错误
-    ERR_RFID_FAULT,         //读卡器获取UID错误
-    ERR_RFID_PE,            //读卡器串口奇偶校验错误
-    ERR_RFID_NE,            //读卡器串口噪声错误
-    ERR_RFID_FE,            //读卡器串口帧错误
-    ERR_RFID_ORE,           //读卡器串口超载
-    ERR_RFID_DMA,           //读卡器串口DMA传输错误
-    ERR_NETMODULE_PE,       //网络模块串口奇偶校验错误
-    ERR_NETMODULE_NE,       //网络模块串口噪声错误
-    ERR_NETMODULE_FE,       //网络模块串口帧错误
-    ERR_NETMODULE_ORE,      //网络模块串口超载
-    ERR_NETMODULE_DMA,      //网络模块串口DMA传输错误
+    ERR_FILE_RW,		//文件读写错误
+    ERR_FILE_NO,		//无目标文件
+    ERR_FILE_PARSE,		//"参数解析错误",
+    ERR_FILE_PARAM,		//参数内容错误
+    ERR_SET_PARAM,		//设置参数范围错误
+    ERR_SET_SERIALIZATION,	//JSON序列化错误
+    ERR_OTHER,			//其他错误
+    ERR_RFID_FAULT,		//读卡器获取UID错误
+    ERR_RFID_PE,		//读卡器串口奇偶校验错误
+    ERR_RFID_NE,		//读卡器串口噪声错误
+    ERR_RFID_FE,		//读卡器串口帧错误
+    ERR_RFID_ORE,		//读卡器串口超载
+    ERR_RFID_DMA,		//读卡器串口DMA传输错误
+    ERR_NETMODULE_PE,		//网络模块串口奇偶校验错误
+    ERR_NETMODULE_NE,		//网络模块串口噪声错误
+    ERR_NETMODULE_FE,		//网络模块串口帧错误
+    ERR_NETMODULE_ORE,		//网络模块串口超载
+    ERR_NETMODULE_DMA,		//网络模块串口DMA传输错误
 
-    ERR_NONET,               //网络通信故障
-    ERR_NET_TIMEOUT,        //网络通信超时
-    ERR_MEMORY                //Malloc错误
+    ERR_NONET,			//网络通信故障
+    ERR_NET_TIMEOUT,		//网络通信超时
+    ERR_MEMORY			//Malloc错误
 
 } ErrorCode_t;
 
@@ -76,25 +76,25 @@ typedef enum _ErrorLevel
 
 typedef struct _ErrorPackage
 {
-    uint32_t ulDevID;
-    ErrorCode_t code;
-    ErrorLevel_t level;
-    uint8_t msg[64];
+    uint32_t		ulDevID;
+    ErrorCode_t		code;
+    ErrorLevel_t	level;
+    uint8_t		msg[64];
 } ErrorPackage_t;
 
 //充电枪ID从小到大定义，其他设备从大小定义
 //DevID 0~? 充电枪ID
-#define defDevID_System     250
-#define defDevID_Cloud      251
-#define defDevID_File       252
-#define defDevID_RFID       253
-#define defDevID_NetMoudle  254
-#define defDevID_EVSE       255 //0xff
+#define defDevID_System		250
+#define defDevID_Cloud		251
+#define defDevID_File		252
+#define defDevID_RFID		253
+#define defDevID_NetMoudle	254
+#define defDevID_EVSE		255	//0xff
 
 
-extern const uint8_t *strErrorCode[];
+extern const uint8_t	*strErrorCode[];
 
-void ThrowErrorCode(uint32_t ulDevID, ErrorCode_t errcode, ErrorLevel_t errlevel, uint8_t *msg);
-void ThrowFSCode (FRESULT rc, uint8_t *path, uint8_t *info);
+void	ThrowErrorCode(uint32_t ulDevID, ErrorCode_t errcode, ErrorLevel_t errlevel, uint8_t *msg);
+void	ThrowFSCode (FRESULT rc, uint8_t *path, uint8_t *info);
 
 #endif
