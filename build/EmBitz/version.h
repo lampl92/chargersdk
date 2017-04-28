d9f120e0332f934ae90d73eeb90ca66b552cc9ca
yy <yuye@dpc.com.cn>
2017-04-28 09:25:55 +0800

    根据硬件更新触摸屏控制引脚

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "27"
+	#define DATE "28"
 	#define MONTH "04"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.04"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 2
 	#define MINOR 7
-	#define BUILD 1872
-	#define REVISION 10186
+	#define BUILD 1874
+	#define REVISION 10190
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 3239
-	#define RC_FILEVERSION 2,7,1872,10186
-	#define RC_FILEVERSION_STRING "2, 7, 1872, 10186\0"
-	#define FULLVERSION_STRING "2.7.1872.10186"
+	#define BUILDS_COUNT 3240
+	#define RC_FILEVERSION 2,7,1874,10190
+	#define RC_FILEVERSION_STRING "2, 7, 1874, 10190\0"
+	#define FULLVERSION_STRING "2.7.1874.10190"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 72
+	#define BUILD_HISTORY 74
 	
 
 #endif //VERSION_H
