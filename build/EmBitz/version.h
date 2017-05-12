fd0fa588dc7bf10f01a3412a8e26d54194e72669
rgw <rgw5267@gmail.com>
2017-05-12 17:07:22 +0800

    project

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "10"
+	#define DATE "12"
 	#define MONTH "05"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.05"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 2
 	#define MINOR 8
-	#define BUILD 1906
-	#define REVISION 10435
+	#define BUILD 1920
+	#define REVISION 10507
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 3290
-	#define RC_FILEVERSION 2,8,1906,10435
-	#define RC_FILEVERSION_STRING "2, 8, 1906, 10435\0"
-	#define FULLVERSION_STRING "2.8.1906.10435"
+	#define BUILDS_COUNT 3309
+	#define RC_FILEVERSION 2,8,1920,10507
+	#define RC_FILEVERSION_STRING "2, 8, 1920, 10507\0"
+	#define FULLVERSION_STRING "2.8.1920.10507"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 6
+	#define BUILD_HISTORY 20
 	
 
 #endif //VERSION_H
