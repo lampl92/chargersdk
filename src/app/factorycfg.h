2bb8ce1672ea60c9c384f23e9f42981390c00e30
rgw <rgw5267@gmail.com>
2017-05-16 09:26:27 +0800

    配置文件恢复出厂设置

--- src/app/factorycfg.h
+++ src/app/factorycfg.h
@@ -8,7 +8,7 @@
 #ifndef  __FACTORYCFG_H
 #define  __FACTORYCFG_H
 
-const uint8_t *strEVSECfg = "{\"EVSESN\":\"0102030405060708\",\"EVSEID\":\"0102030405060708\",\"EVSEType\":2,\"TotalCON\":1,\"Lng\":116.275833,\"Lat\":39.831944,\"ServiceFeeType\":1,\"ServiceFee\":1,\"DefSegFee\":1.2,\"CONArray\":[{\"ID\":0,\"Type\":2,\"SocketType\":66,\"VolatageUpperLimits\":250,\"VolatageLowerLimits\":190,\"ACTempUpperLimits\":105,\"ACTempLowerLimits\":-40,\"SocketTempUpperLimits\":105,\"SocketTempLowerLimits\":-40,\"RatedCurrent\":32,\"RatedPower\":7}],\"TemplSegArray\":[]}";
+const uint8_t *strEVSECfg = "{\"EVSESN\":\"0102030405060708\",\"EVSEID\":\"0102030405060708\",\"EVSEType\":2,\"TotalCON\":1,\"Lng\":116.275833,\"Lat\":39.831944,\"ServiceFeeType\":1,\"ServiceFee\":1,\"DefSegFee\":1.2,\"CONArray\":[{\"ID\":0,\"Type\":2,\"SocketType\":66,\"VolatageUpperLimits\":250,\"VolatageLowerLimits\":190,\"ACTempUpperLimits\":105,\"ACTempLowerLimits\":-40,\"SocketTempUpperLimits\":105,\"SocketTempLowerLimits\":-40,\"RatedCurrent\":32,\"RatedPower\":7,\"QRCode\":\"DPC\"}],\"TemplSegArray\":[]}";
 
 /*
 {
@@ -33,7 +33,8 @@ const uint8_t *strEVSECfg = "{\"EVSESN\":\"0102030405060708\",\"EVSEID\":\"01020
             "SocketTempUpperLimits": 105,
             "SocketTempLowerLimits": -40,
             "RatedCurrent": 32,
-            "RatedPower": 7
+            "RatedPower": 7,
+            "QRCode":"DPC"
         }
     ],
     "TemplSegArray": []
