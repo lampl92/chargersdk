88becb007e46caf39e2ad8f70de99ac9aaf6632d
rgw <rgw5267@gmail.com>
2017-05-17 14:37:29 +0800

    int or short to bytes

--- src/middlewares/userlib/src/utils.h
+++ src/middlewares/userlib/src/utils.h
@@ -8,7 +8,19 @@
 #ifndef  __UTILS_H
 #define  __UTILS_H
 
+#include "stm32f4xx.h"
 
+typedef union
+{
+	uint32_t ulVal;
+	uint8_t ucVal[4];
+}ul2uc;
+
+typedef union
+{
+	uint16_t usVal;
+	uint8_t ucVal[2];
+}us2uc;
 uint32_t HexToStr(uint8_t *Hex, uint8_t *Src, int Hexlen);
 
 #endif
