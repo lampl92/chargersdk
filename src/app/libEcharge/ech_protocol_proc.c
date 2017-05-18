9cb217409f74a21250775e290bd1097214bc06c0
rgw <rgw5267@gmail.com>
2017-05-18 11:03:05 +0800

    协议处理函数

---
+++ src/app/libEcharge/ech_protocol_proc.c
@@ -0,0 +1,9 @@
+/**
+* @file ech_protocol_proc.c
+* @brief
+* @author rgw
+* @version v1.0
+* @date 2017-02-21
+*/
+#include "libEcharge/ech_err.h"
+
---
+++ src/app/libEcharge/ech_protocol_proc.h
@@ -0,0 +1,24 @@
+/**
+* @file D:\Documents\Projects\chargersdk\src\app\libEcharge\ech_protocol_proc.h
+* @brief
+* @author rgw
+* @version v1.0
+* @date 2017-05-18
+*/
+#ifndef  __ECH_PROTOCOL_PROC_H
+#define  __ECH_PROTOCOL_PROC_H
+
+#include "ech_protocol.h"
+
+typedef struct
+{
+    time_t timestamp;
+    uint32_t timeout;
+    echCMDType_t cmd;
+    uint32_t len;
+    uint32_t *buff;
+    uint8_t status;
+}
+
+
+#endif
