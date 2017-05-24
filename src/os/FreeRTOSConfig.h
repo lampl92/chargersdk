54df6dd7836e53e00b30b4bf42ba8ccc9889e1ba
rgw <rgw5267@gmail.com>
2017-05-24 09:41:32 +0800

    Timer Queue长度调整

--- src/os/FreeRTOSConfig.h
+++ src/os/FreeRTOSConfig.h
@@ -137,7 +137,7 @@
 /* Software timer definitions. */
 #define configUSE_TIMERS				1
 #define configTIMER_TASK_PRIORITY		( 22 )
-#define configTIMER_QUEUE_LENGTH		16
+#define configTIMER_QUEUE_LENGTH		32
 #define configTIMER_TASK_STACK_DEPTH	( configMINIMAL_STACK_SIZE * 2 )
 
 /* Set the following definitions to 1 to include the API function, or zero
