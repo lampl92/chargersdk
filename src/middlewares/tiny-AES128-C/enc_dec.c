d31a97950046b02b7d63212dbfb96433774c711e
rgw <rgw5267@gmail.com>
2017-05-24 09:35:24 +0800

    修正操作buff越界

--- src/middlewares/tiny-AES128-C/enc_dec.c
+++ src/middlewares/tiny-AES128-C/enc_dec.c
@@ -1,45 +1,48 @@
 #include <string.h>
 #include "aes.h"
 
-/** @brief
+/** @brief aes cbc 加密
  *
- * @param input char*
- * @param inputSize int
- * @param key const char*
-            buffer of key
- * @param output char*
-            buffer of output
- * @return int
-                Return output length
+ * @param input char*       明文
+ * @param inputSize int     明文长度
+ * @param key const char*   16位Key
+ * @param output char*      密文
+ * @return int              密文长度
  *
  */
 int aes_encrypt(char *input, int inputSize, const char *key, char *output)
 {
-    //assert(NULL != input && NULL != output);
-    //assert(inputSize <=1024 && inputSize > 0);
-    int pendLen = 16 - inputSize % 16;
     unsigned char iv[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
     char temp[1024] = {0};
+    int pendLen;
+
     memcpy(temp, input, inputSize);
-    memset(temp + inputSize, pendLen, pendLen);
+    if(inputSize % 16 == 0)
+    {
+        pendLen = 0;
+    }
+    else
+    {
+        pendLen = 16 - inputSize % 16;
+        memset(temp + inputSize, pendLen, pendLen);
+    }
+
     AES128_CBC_encrypt_buffer((uint8_t *)output, (uint8_t *)temp, inputSize + pendLen, (uint8_t *)key, iv);
+
     return inputSize + pendLen;
 }
 
-/** @brief
+/** @brief aes cbc 解密
  *
- * @param input char*
- * @param key const char*
- * @param output char*
- * @param outputSize int
+ * @param input char*       密文
+ * @param key const char*   16位Key
+ * @param output char*      明文
+ * @param outputSize int    密文长度
  * @return void
  *
  */
 void aes_decrypt(char *input, const char *key, char *output, int outputSize)
 {
-    //assert(NULL != input && NULL != output);
     unsigned char iv[16] = {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
-    int pendLen = 16 - outputSize % 16;
-    AES128_CBC_decrypt_buffer((uint8_t *)output, (uint8_t *)input, outputSize + pendLen, (uint8_t *)key, iv);
-    memset(output + outputSize, 0, pendLen);
+    AES128_CBC_decrypt_buffer((uint8_t *)output, (uint8_t *)input, outputSize, (uint8_t *)key, iv);
 }
