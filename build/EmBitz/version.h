c7b717818c78059fc3a113f48065bd97649015ee
rgw <rgw5267@gmail.com>
2017-05-24 10:11:45 +0800

    登陆与心跳【编译】

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "17"
+	#define DATE "24"
 	#define MONTH "05"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.05"
@@ -13,18 +13,18 @@
 	
 	//Standard Version Type
 	#define MAJOR 2
-	#define MINOR 8
-	#define BUILD 1958
-	#define REVISION 10707
+	#define MINOR 9
+	#define BUILD 2078
+	#define REVISION 11292
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 3357
-	#define RC_FILEVERSION 2,8,1958,10707
-	#define RC_FILEVERSION_STRING "2, 8, 1958, 10707\0"
-	#define FULLVERSION_STRING "2.8.1958.10707"
+	#define BUILDS_COUNT 3587
+	#define RC_FILEVERSION 2,9,2078,11292
+	#define RC_FILEVERSION_STRING "2, 9, 2078, 11292\0"
+	#define FULLVERSION_STRING "2.9.2078.11292"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 58
+	#define BUILD_HISTORY 78
 	
 
 #endif //VERSION_H
