8ae28e8a92f778a0febee52422acf59bae696cbb
rgw <rgw5267@gmail.com>
2017-06-05 09:24:12 +0800

    调试时关闭看门狗

--- src/drivers/bsp/src/bsp.c
+++ src/drivers/bsp/src/bsp.c
@@ -141,7 +141,9 @@ void bsp_Init(void)
     //bsp_LTDC_Init();//在GUI中初始化
 //    bsp_Touch_Init();
     bsp_Uart_Init();   /* 初始化串口 */
+#ifndef EVSE_DEBUG
     IWDG_Init(IWDG_PRESCALER_64,500);  	//·ÖÆµÊýÎª64,ÖØÔØÖµÎª500,Òç³öÊ±¼äÎª1s
+#endif
 //LCD_Init();
 
 }
