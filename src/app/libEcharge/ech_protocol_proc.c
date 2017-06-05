fec3e6218f11d2a7f7da035714f3de32294df4db
rgw <rgw5267@gmail.com>
2017-06-05 20:13:57 +0800

    断线重连机制

--- src/app/libEcharge/ech_protocol_proc.c
+++ src/app/libEcharge/ech_protocol_proc.c
@@ -33,8 +33,9 @@ void vTaskRemoteCmdProc(void *pvParameters)
     cr = gdsl_list_cursor_alloc (pProto->plechRecvCmd);
     while(1)
     {
-//        printf_safe("send elem = %d\n", gdsl_list_get_size(pProto->plechSendCmd));
-//        printf_safe("recv elem = %d\n", gdsl_list_get_size(pProto->plechRecvCmd));
+        printf_safe("send elem = %d\n", gdsl_list_get_size(pProto->plechSendCmd));
+        printf_safe("recv elem = %d\n", gdsl_list_get_size(pProto->plechRecvCmd));
+        printf_safe("\n");
 
         /** @todo (rgw#1#): ！！！ 需要抽时间整理一下这里的超时和重发次数，逻辑上有问题 */
 
@@ -59,7 +60,7 @@ void vTaskRemoteCmdProc(void *pvParameters)
                 else//没发出去
                 {
                     pechCmdElem->trycount++;
-                    pppClose(ppp);
+                    pppSigHUP(ppp);
                     if(pechCmdElem->trycount > pechCmdElem->trycountmax)
                     {
                         gdsl_list_cursor_delete(cs);
