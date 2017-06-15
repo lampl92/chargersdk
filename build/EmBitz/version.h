b54749c9f13e5350c9ff9302f1d08677eb4e9bfe
rgw <rgw5267@gmail.com>
2017-06-15 19:27:15 +0800

    【编译】命令模式

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 3
-	#define BUILD 2508
-	#define REVISION 13653
+	#define BUILD 2543
+	#define REVISION 13841
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 4343
-	#define RC_FILEVERSION 3,3,2508,13653
-	#define RC_FILEVERSION_STRING "3, 3, 2508, 13653\0"
-	#define FULLVERSION_STRING "3.3.2508.13653"
+	#define BUILDS_COUNT 4415
+	#define RC_FILEVERSION 3,3,2543,13841
+	#define RC_FILEVERSION_STRING "3, 3, 2543, 13841\0"
+	#define FULLVERSION_STRING "3.3.2543.13841"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 8
+	#define BUILD_HISTORY 43
 	
 
 #endif //VERSION_H
