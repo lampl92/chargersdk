46737559976943116e07a5c2810628874208c8a8
zshare <sunnyshanchen@163.com>
2017-06-16 16:33:52 +0800

    字体改为以前的了

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 3
-	#define BUILD 2572
-	#define REVISION 13994
+	#define BUILD 2573
+	#define REVISION 14001
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 4465
-	#define RC_FILEVERSION 3,3,2572,13994
-	#define RC_FILEVERSION_STRING "3, 3, 2572, 13994\0"
-	#define FULLVERSION_STRING "3.3.2572.13994"
+	#define BUILDS_COUNT 4467
+	#define RC_FILEVERSION 3,3,2573,14001
+	#define RC_FILEVERSION_STRING "3, 3, 2573, 14001\0"
+	#define FULLVERSION_STRING "3.3.2573.14001"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 72
+	#define BUILD_HISTORY 73
 	
 
 #endif //VERSION_H
