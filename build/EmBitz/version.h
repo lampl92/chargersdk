fd3d3ced76e28d529d5c8522a7365bae9b00e92b
zshare <sunnyshanchen@163.com>
2017-06-19 10:22:23 +0800

    增加libm.a

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "16"
+	#define DATE "19"
 	#define MONTH "06"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.06"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 3
-	#define BUILD 2575
-	#define REVISION 14017
+	#define BUILD 2581
+	#define REVISION 14041
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 4472
-	#define RC_FILEVERSION 3,3,2575,14017
-	#define RC_FILEVERSION_STRING "3, 3, 2575, 14017\0"
-	#define FULLVERSION_STRING "3.3.2575.14017"
+	#define BUILDS_COUNT 4477
+	#define RC_FILEVERSION 3,3,2581,14041
+	#define RC_FILEVERSION_STRING "3, 3, 2581, 14041\0"
+	#define FULLVERSION_STRING "3.3.2581.14041"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 75
+	#define BUILD_HISTORY 81
 	
 
 #endif //VERSION_H
