77171d57570000e8690615831071e678de96e161
zshare <sunnyshanchen@163.com>
2017-06-19 17:41:22 +0800

    楼下充电桩，2017年6月19日

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -13,18 +13,18 @@
 	
 	//Standard Version Type
 	#define MAJOR 3
-	#define MINOR 3
-	#define BUILD 2581
-	#define REVISION 14041
+	#define MINOR 4
+	#define BUILD 2602
+	#define REVISION 14164
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 4477
-	#define RC_FILEVERSION 3,3,2581,14041
-	#define RC_FILEVERSION_STRING "3, 3, 2581, 14041\0"
-	#define FULLVERSION_STRING "3.3.2581.14041"
+	#define BUILDS_COUNT 4518
+	#define RC_FILEVERSION 3,4,2602,14164
+	#define RC_FILEVERSION_STRING "3, 4, 2602, 14164\0"
+	#define FULLVERSION_STRING "3.4.2602.14164"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 81
+	#define BUILD_HISTORY 2
 	
 
 #endif //VERSION_H
