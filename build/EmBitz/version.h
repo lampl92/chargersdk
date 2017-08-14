6b177ce10c5f9a758b68a9082f976abd92903d8f
zshare <sunnyshanchen@163.com>
2017-08-14 08:52:31 +0800

    版本

--- build/EmBitz/version.h
+++ build/EmBitz/version.h
@@ -2,29 +2,29 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "29"
-	#define MONTH "06"
+	#define DATE "04"
+	#define MONTH "08"
 	#define YEAR "2017"
-	#define UBUNTU_VERSION_STYLE "17.06"
+	#define UBUNTU_VERSION_STYLE "17.08"
 	
 	//Software Status
 	#define STATUS "Beta"
 	#define STATUS_SHORT "b"
 	
 	//Standard Version Type
-	#define MAJOR 3
-	#define MINOR 7
-	#define BUILD 2956
-	#define REVISION 16027
+	#define MAJOR 4
+	#define MINOR 1
+	#define BUILD 3475
+	#define REVISION 18833
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 5166
-	#define RC_FILEVERSION 3,7,2956,16027
-	#define RC_FILEVERSION_STRING "3, 7, 2956, 16027\0"
-	#define FULLVERSION_STRING "3.7.2956.16027"
+	#define BUILDS_COUNT 6154
+	#define RC_FILEVERSION 4,1,3475,18833
+	#define RC_FILEVERSION_STRING "4, 1, 3475, 18833\0"
+	#define FULLVERSION_STRING "4.1.3475.18833"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 56
+	#define BUILD_HISTORY 75
 	
 
 #endif //VERSION_H
