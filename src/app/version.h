84e9bf066e435bfc8a054e0ef0f45190cee3e086
rgw <rgw5267@gmail.com>
2017-08-18 17:32:36 +0800

    升级hal

--- src/app/version.h
+++ src/app/version.h
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 10
-	#define BUILD 3208
-	#define REVISION 17416
+	#define BUILD 3211
+	#define REVISION 17433
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 5526
-	#define RC_FILEVERSION 3,10,3208,17416
-	#define RC_FILEVERSION_STRING "3, 10, 3208, 17416\0"
-	#define FULLVERSION_STRING "3.10.3208.17416"
+	#define BUILDS_COUNT 5529
+	#define RC_FILEVERSION 3,10,3211,17433
+	#define RC_FILEVERSION_STRING "3, 10, 3211, 17433\0"
+	#define FULLVERSION_STRING "3.10.3211.17433"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 8
+	#define BUILD_HISTORY 11
 	
 
 #endif //VERSION_H
