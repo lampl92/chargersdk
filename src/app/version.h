5d363cfbe7fd4625ddee96804344513bac960e09
rgw <rgw5267@gmail.com>
2017-08-23 16:11:08 +0800

    project

--- src/app/version.h
+++ src/app/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "18"
+	#define DATE "23"
 	#define MONTH "08"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.08"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 10
-	#define BUILD 3212
-	#define REVISION 17438
+	#define BUILD 3215
+	#define REVISION 17460
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 5532
-	#define RC_FILEVERSION 3,10,3212,17438
-	#define RC_FILEVERSION_STRING "3, 10, 3212, 17438\0"
-	#define FULLVERSION_STRING "3.10.3212.17438"
+	#define BUILDS_COUNT 5541
+	#define RC_FILEVERSION 3,10,3215,17460
+	#define RC_FILEVERSION_STRING "3, 10, 3215, 17460\0"
+	#define FULLVERSION_STRING "3.10.3215.17460"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 12
+	#define BUILD_HISTORY 15
 	
 
 #endif //VERSION_H
