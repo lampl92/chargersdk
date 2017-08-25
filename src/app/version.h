641eb4a1d16affda0c436cdbb071e8137f25039f
rgw <rgw5267@gmail.com>
2017-08-25 09:28:49 +0800

    工程增加cli_crc.c文件

--- src/app/version.h
+++ src/app/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "23"
+	#define DATE "24"
 	#define MONTH "08"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.08"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 10
-	#define BUILD 3215
-	#define REVISION 17460
+	#define BUILD 3216
+	#define REVISION 17466
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 5541
-	#define RC_FILEVERSION 3,10,3215,17460
-	#define RC_FILEVERSION_STRING "3, 10, 3215, 17460\0"
-	#define FULLVERSION_STRING "3.10.3215.17460"
+	#define BUILDS_COUNT 5542
+	#define RC_FILEVERSION 3,10,3216,17466
+	#define RC_FILEVERSION_STRING "3, 10, 3216, 17466\0"
+	#define FULLVERSION_STRING "3.10.3216.17466"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 15
+	#define BUILD_HISTORY 16
 	
 
 #endif //VERSION_H
