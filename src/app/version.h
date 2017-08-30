cd00a56357a53a5da22bc5019dcc20ce223af6b7
rgw <rgw5267@gmail.com>
2017-08-30 09:09:37 +0800

    为vs工程添加hmi文件

--- src/app/version.h
+++ src/app/version.h
@@ -2,7 +2,7 @@
 #define VERSION_H
 
 	//Date Version Types
-	#define DATE "24"
+	#define DATE "30"
 	#define MONTH "08"
 	#define YEAR "2017"
 	#define UBUNTU_VERSION_STYLE "17.08"
@@ -14,17 +14,17 @@
 	//Standard Version Type
 	#define MAJOR 3
 	#define MINOR 10
-	#define BUILD 3216
-	#define REVISION 17466
+	#define BUILD 3219
+	#define REVISION 17484
 	
 	//Miscellaneous Version Types
-	#define BUILDS_COUNT 5542
-	#define RC_FILEVERSION 3,10,3216,17466
-	#define RC_FILEVERSION_STRING "3, 10, 3216, 17466\0"
-	#define FULLVERSION_STRING "3.10.3216.17466"
+	#define BUILDS_COUNT 5545
+	#define RC_FILEVERSION 3,10,3219,17484
+	#define RC_FILEVERSION_STRING "3, 10, 3219, 17484\0"
+	#define FULLVERSION_STRING "3.10.3219.17484"
 	
 	//These values are to keep track of your versioning state, don't modify them.
-	#define BUILD_HISTORY 16
+	#define BUILD_HISTORY 19
 	
 
 #endif //VERSION_H
