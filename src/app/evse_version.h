ccdee396e21b8df27e90e9a6ff49b08a33ae422b
rgw <rgw5267@gmail.com>
2017-12-22 17:26:51 +0800

    判断配置文件版本如果与程序版本不一致,则修改配置文件版本

--- src/app/evse_version.h
+++ src/app/evse_version.h
@@ -1,14 +1,19 @@
-#ifndef EVSE_VERSION_H
-#define EVSE_VERSION_H
+#ifndef EVSE_VERSION_01_03_1222_H
+#define EVSE_VERSION_01_03_1222_H
+
+//                   ↑   ↑  ↑
+//                   |   |  |
+//<----!!!用于生成远程升级文件夹, 一定要与下面版本号一致, 不一致就揍了
+
+
+    #define EVSE_MAJOR          01              //主版本,两位, 对应生产用程序ABCD版本
+    #define EVSE_MINOR          03              //副版本,两位, 对应PDM A1,A2版本
+    #define EVSE_PATCH          1222            //补丁版本,四位, 目前用作构建日期, 月日表示
 	
-    #define EVSE_MAJOR 01            //主版本, 对应生产用程序ABCD版本
-    #define EVSE_MINOR 03            //副版本, 对应PDM A1,A2版本
-    #define EVSE_PATCH 1220          //补丁版本, 目前用作构建日期, 月日表示
-    #define EVSE_TARGET 1639         //项目号,订单号,整改号
 	
-    #define EVSE_VERSION_STRING "1639 01.03.1220"
-	
-    #define EVSE_BUILD_AUTHOR "RGW"
+    #define EVSE_TARGET         "S1639"         //项目号,订单号,整改号
+    #define EVSE_BUILD_AUTHOR   "RGW"
+    //#define EVSE_VERSION_STRING "S1639 01.03.1222"
 	
 
 #endif //VERSION_H
