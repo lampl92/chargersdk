0ec812d055d468a07b0b1830163ac1acd2770b21
rgw <rgw5267@gmail.com>
2018-01-17 09:42:19 +0800

    dummy

--- src/app/connector.c
+++ src/app/connector.c
@@ -726,7 +726,7 @@ static ErrorCode_t GetCPState(void *pvCON)
 
     if(ucCONID == 0)
     {
-#ifndef DEBUG_DIAG_DUMMY
+#ifdef DEBUG_DIAG_DUMMY
         tmpCPState = CP_6V_PWM;
 #else
         cp1 = Sys_samp.DC.CP1;//get_CP1();
