4574b773b89fb2441e634af3e412b02f05477ded
y2603012723 <y26030012723@163.com>
2018-01-26 08:44:52 +0800

    修改触摸驱动配置缺陷

--- src/middlewares/STemWin/GUI_X/GUI_X_Touch_Analog.c
+++ src/middlewares/STemWin/GUI_X/GUI_X_Touch_Analog.c
@@ -58,7 +58,7 @@ static uint8_t _pidFlag = 0;
 static uint16_t _pidCount = 0;
 static uint32_t _AsmtCount = 0;//进入广告时间计数
 
-static uint32_t _pointpause = 0;//进入广告时间计数
+//static uint32_t _pointpause = 0;//
 
 extern uint16_t calebrate_done;
 
@@ -87,15 +87,15 @@ void GUI_TOUCH_X_ActivateX(void)
         }
         else
         {
-            _pointpause++;
-            if (_pointpause > 150)
-            {
-                State.x = 795;
-                State.y = 475;
+//            _pointpause++;
+//            if (_pointpause > 150)
+//            {
+            State.x = adc_x;
+            State.y = adc_y;
                 State.Pressed = 0;
                 GUI_TOUCH_StoreStateEx(&State);
-                _pointpause = 0;
-            }
+//                _pointpause = 0;
+//            }
             if ((cur_win == _hWinHome) || (cur_win == _hWinCharging))
             {
                 _AsmtCount++;
