3700711ea7f37a59222618f35bd62144763be128
zshare <sunnyshanchen@163.com>
2018-01-31 19:07:19 +0800

    增大任务名

--- src/os/FreeRTOS/FreeRTOSConfig.h
+++ src/os/FreeRTOS/FreeRTOSConfig.h
@@ -100,7 +100,7 @@
 #define configMAX_PRIORITIES			( 32 )  //0 is lowest
 #define configMINIMAL_STACK_SIZE		( ( unsigned short ) 512 )
 #define configTOTAL_HEAP_SIZE			( ( size_t ) ( 20*1024 * 1024 ) )
-#define configMAX_TASK_NAME_LEN			( 16 )
+#define configMAX_TASK_NAME_LEN			( 32 )
 #define configUSE_TRACE_FACILITY		1
 #define configUSE_16_BIT_TICKS			0
 #define configIDLE_SHOULD_YIELD			1
