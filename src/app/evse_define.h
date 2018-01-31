e39117b3bbdad5e6628e11115bd6c2bfcd71ca40
zshare <sunnyshanchen@163.com>
2018-01-31 15:11:28 +0800

    删除无用信号, 只保留一个单独relay_paste信号

--- src/app/evse_define.h
+++ src/app/evse_define.h
@@ -79,7 +79,7 @@
 #define defEventBitOrderStopTypeUnPlug           BIT_15
 
 #define defEventBitOrderMakeFinish               BIT_16  //等待处不清除, 该事件置位后整个订单完成
-#define defEventBitOrderFinishToChargetask       BIT_17
+//#define defEventBitOrderFinishToChargetask       BIT_17
 #define defEventBitOrderFinishToHMI              BIT_18
 
 #define defEventBitOrderTmpTimer                 BIT_19
@@ -136,7 +136,7 @@
 #define defEventBitExceptionChargeTimer BIT_6
 #define defEventBitExceptionRFID        BIT_7
 #define defEventBitExceptionMeter       BIT_8
-#define defEventBitExceptionRelayPaste  BIT_9
+//#define defEventBitExceptionRelayPaste  BIT_9
 
 #define defEventBitExceptionLimitFee    BIT_10  //把LimitFee放在这里，Exception名字虽说有点不搭，但都是满足条件即停止充电。
 #define defEventBitExceptionLimitTime   BIT_16  //把LimitTime放在这里，Exception名字虽说有点不搭，但都是满足条件即停止充电。
@@ -146,7 +146,8 @@
 #define defEventBitExceptionTempSensor  BIT_14
 #define defEventBitExceptionSocketTempSensor  BIT_15
 
-#define defEventBitExceptionDevFault    (defEventBitExceptionRFID | defEventBitExceptionMeter|defEventBitExceptionRelayPaste)
+#define defEventBitExceptionDevFault    (defEventBitExceptionRFID | \
+                                         defEventBitExceptionMeter)
 
 /*------pCON->status.xHandleEventCharge*/
 #define defEventBitCONAuthed            BIT_0       //帐户认证OK
@@ -306,18 +307,19 @@
 #define defSignalCON_Fault_AC_B_Temp            BIT_2
 #define defSignalCON_Fault_AC_C_Temp            BIT_3
 #define defSignalCON_Fault_AC_N_Temp            BIT_4
-#define defSignalCON_Fault_AC_A_RelayPaste      BIT_5
-#define defSignalCON_Fault_AC_B_RelayPaste      BIT_6
-#define defSignalCON_Fault_AC_C_RelayPaste      BIT_7
-#define defSignalCON_Fault_AC_N_RelayPaste      BIT_8
+#define defSignalCON_Fault_RelayPaste           BIT_5
+//#define defSignalCON_Fault_AC_B_RelayPaste      BIT_6
+//#define defSignalCON_Fault_AC_C_RelayPaste      BIT_7
+//#define defSignalCON_Fault_AC_N_RelayPaste      BIT_8
 #define defSignalCON_Fault_CP                   BIT_9        //CP传感故障
 #define defSignalCON_Fault_Plug                 BIT_10
 #define defSignalCON_Fault_Meter                BIT_11       //电表或电能芯片
 
-#define defSignalGroupCON_Fault_AC_RelayPase       (defSignalCON_Fault_AC_A_RelayPaste | \
-                                                    defSignalCON_Fault_AC_B_RelayPaste | \
-                                                    defSignalCON_Fault_AC_C_RelayPaste | \
-                                                    defSignalCON_Fault_AC_N_RelayPaste )
+//#define defSignalGroupCON_Fault_AC_RelayPaste       (defSignalCON_Fault_AC_A_RelayPaste | \
+//                                                    defSignalCON_Fault_AC_B_RelayPaste | \
+//                                                    defSignalCON_Fault_AC_C_RelayPaste | \
+//                                                    defSignalCON_Fault_AC_N_RelayPaste )
+    
 #define defSignalGroupCON_Alarm_Temp_War           (defSignalCON_Alarm_SocketTemp1_War | \
                                                 defSignalCON_Alarm_SocketTemp2_War | \
                                                 defSignalCON_Alarm_AC_A_Temp_War | \
