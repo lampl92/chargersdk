96a1c086a1f5c9f8a0f6c13175681ea328c0a2b8
rgw <rgw5267@gmail.com>
2018-04-16 16:05:24 +0800

    release:01.04.0416

--- src/app/evse_version.h
+++ src/app/evse_version.h
@@ -1,5 +1,5 @@
-#ifndef EVSE_VERSION_01_04_0409_H
-#define EVSE_VERSION_01_04_0409_H
+#ifndef EVSE_VERSION_01_04_0416_H
+#define EVSE_VERSION_01_04_0416_H
 
 //                   ↑   ↑  ↑
 //                   |   |  |
@@ -8,7 +8,7 @@
 
     #define EVSE_MAJOR          "01"              //主版本,两位, 对应生产用程序ABCD版本
     #define EVSE_MINOR          "04"              //副版本,两位, 对应PDM A1,A2版本
-    #define EVSE_PATCH          "0409"            //补丁版本,四位, 目前用作构建日期, 月日表示
+    #define EVSE_PATCH          "0416"            //补丁版本,四位, 目前用作构建日期, 月日表示
 	
 	
     #define EVSE_TARGET         "S1639"         //项目号,订单号,整改号
