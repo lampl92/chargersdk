6c2c066736c8c7f4a0d1aecfb582e9433f4d7a4f
rgw <rgw5267@gmail.com>
2018-06-01 14:56:42 +0800

    增加PhaseLine 合并1639-1配置文件 (cherry picked from commit 463abee0cec30b86ad9c8a2b1aadf137afca1693)

--- src/app/factorycfg.c
+++ src/app/factorycfg.c
@@ -9,110 +9,125 @@
 
 const char *strEVSECfg = 
 "{\
-    \"EVSESN\":\"0102030405060708\",\
-    \"EVSEID\":\"3000000000000002\",\
-    \"EVSEType\":2,\
-    \"TotalCON\":1,\
-    \"Lng\":116.275833,\
-    \"Lat\":39.831944,\
-    \"ServiceFeeType\":1,\
-    \"ServiceFee\":1,\
-    \"DefSegFee\":1.2,\
-    \"CONArray\":[\
-        {\
-            \"ID\":0,\
-            \"Type\":2,\
-            \"SocketType\":67,\
-            \"VolatageUpperLimits\":260,\
-            \"VolatageLowerLimits\":176,\
-            \"ACTempUpperLimits\":95,\
-            \"ACTempLowerLimits\":-40,\
-            \"SocketTempUpperLimits\":95,\
-            \"SocketTempLowerLimits\":-40,\
-            \"RatedCurrent\":32,\
-            \"RatedPower\":7,\
-            \"QRCode\":\"3000000000000002\"\
-        }\
-    ],\
-    \"TemplSegArray\":[]\
-}";
+    \"EVSESN\":\"0102030405060708\",\n \
+    \"EVSEID\":\"2000000000000002\",\n \
+    \"EVSEType\":2,\n \
+    \"PhaseLine\":1,\n \
+    \"TotalCON\":1,\n \
+    \"Lng\":116.275833,\n \
+    \"Lat\":39.831944,\n \
+    \"ServiceFeeType\":1,\n \
+    \"ServiceFee\":1,\n \
+    \"DefSegFee\":1.2,\n \
+    \"CONArray\":[\n \
+        {\n \
+            \"ID\":0,\n \
+            \"Type\":2,\n \
+            \"SocketType\":67,\n \
+            \"VolatageUpperLimits\":260,\n \
+            \"VolatageLowerLimits\":176,\n \
+            \"ACTempUpperLimits\":95,\n \
+            \"ACTempLowerLimits\":-40,\n \
+            \"SocketTempUpperLimits\":95,\n \
+            \"SocketTempLowerLimits\":-40,\n \
+            \"RatedCurrent\":32,\n \
+            \"RatedPower\":7,\n \
+            \"QRCode\":\"2000000000000002\"\n \
+        },\n \
+        {\n \
+            \"ID\":1,\n \
+            \"Type\":2,\n \
+            \"SocketType\":67,\n \
+            \"VolatageUpperLimits\":260,\n \
+            \"VolatageLowerLimits\":176,\n \
+            \"ACTempUpperLimits\":95,\n \
+            \"ACTempLowerLimits\":-40,\n \
+            \"SocketTempUpperLimits\":95,\n \
+            \"SocketTempLowerLimits\":-40,\n \
+            \"RatedCurrent\":32,\n \
+            \"RatedPower\":7,\n \
+            \"QRCode\":\"2000000000000013\"\n \
+        }\n \
+    ],\n \
+    \"TemplSegArray\":[]\n \
+}\n";
 
 const char *strProtoCfg =
 "{\
-    \"ServerIP\": \"123.56.113.123\",\
-    \"ServerPort\": 6677,\
-    \"UserName\": \"esaasusr\",\
-    \"UserPwd\": \"esaaspasswrd\",\
-    \"Key\": \"0123456789abcdeg\",\
-    \"NewKey\": \"\",\
-    \"NewKeyChangeTime\": 4294967295,\
-    \"OptSN\": 0,\
-    \"ProtoVer\": 104,\
-    \"HeartBeatCyc_ms\": 15000,\
-    \"StatusCyc_ms\": 120000,\
-    \"RTDataCyc_ms\": 10000,\
-    \"ResetAct\": 0,\
-    \"PowerFee_sharp\": 1,\
-    \"PowerFee_peak\": 0,\
-    \"PowerFee_shoulder\": 0,\
-    \"PowerFee_off_peak\": 0,\
-    \"ServiceFee_sharp\": 0.8,\
-    \"ServiceFee_peak\": 0,\
-    \"ServiceFee_shoulder\": 0,\
-    \"ServiceFee_off_peak\": 0,\
-    \"SegTime_sharp\": {\
-        \"SegCont\": 1,\
-        \"Start1\": 0,\
-        \"End1\": 24,\
-        \"Start2\": 255,\
-        \"End2\": 255,\
-        \"Start3\": 255,\
-        \"End3\": 255,\
-        \"Start4\": 255,\
-        \"End4\": 255,\
-        \"Start5\": 255,\
-        \"End5\": 255\
-    },\
-    \"SegTime_peak\": {\
-        \"SegCont\": 0,\
-        \"Start1\": 255,\
-        \"End1\": 255,\
-        \"Start2\": 255,\
-        \"End2\": 255,\
-        \"Start3\": 255,\
-        \"End3\": 255,\
-        \"Start4\": 255,\
-        \"End4\": 255,\
-        \"Start5\": 255,\
-        \"End5\": 255\
-    },\
-    \"SegTime_shoulder\": {\
-        \"SegCont\": 0,\
-        \"Start1\": 255,\
-        \"End1\": 255,\
-        \"Start2\": 255,\
-        \"End2\": 255,\
-        \"Start3\": 255,\
-        \"End3\": 255,\
-        \"Start4\": 255,\
-        \"End4\": 255,\
-        \"Start5\": 255,\
-        \"End5\": 255\
-    },\
-    \"SegTime_off_peak\": {\
-        \"SegCont\": 0,\
-        \"Start1\": 255,\
-        \"End1\": 255,\
-        \"Start2\": 255,\
-        \"End2\": 255,\
-        \"Start3\": 255,\
-        \"End3\": 255,\
-        \"Start4\": 255,\
-        \"End4\": 255,\
-        \"Start5\": 255,\
-        \"End5\": 255\
-    }\
-}";
+    \"ServerIP\": \"123.56.113.123\",\n \
+    \"ServerPort\": 6677,\n \
+    \"UserName\": \"esaasusr\",\n \
+    \"UserPwd\": \"esaaspasswrd\",\n \
+    \"Key\": \"0123456789abcdeg\",\n \
+    \"NewKey\": \"\",\n \
+    \"NewKeyChangeTime\": 2147483647,\n \
+    \"OptSN\": 0,\n \
+    \"ProtoVer\": 104,\n \
+    \"HeartBeatCyc_ms\": 15000,\n \
+    \"StatusCyc_ms\": 120000,\n \
+    \"RTDataCyc_ms\": 10000,\n \
+    \"ResetAct\": 0,\n \
+    \"PowerFee_sharp\": 1,\n \
+    \"PowerFee_peak\": 1,\n \
+    \"PowerFee_shoulder\": 1,\n \
+    \"PowerFee_off_peak\": 1,\n \
+    \"ServiceFee_sharp\": 0.8,\n \
+    \"ServiceFee_peak\": 0.8,\n \
+    \"ServiceFee_shoulder\": 0.8,\n \
+    \"ServiceFee_off_peak\": 0.8,\n \
+    \"SegTime_sharp\": {\n \
+        \"SegCont\": 2,\n \
+        \"Start1\": 11,\n \
+        \"End1\": 13,\n \
+        \"Start2\": 16,\n \
+        \"End2\": 17,\n \
+        \"Start3\": 255,\n \
+        \"End3\": 255,\n \
+        \"Start4\": 255,\n \
+        \"End4\": 255,\n \
+        \"Start5\": 255,\n \
+        \"End5\": 255\n \
+    },\n \
+    \"SegTime_peak\": {\n \
+        \"SegCont\": 3,\n \
+        \"Start1\": 10,\n \
+        \"End1\": 11,\n \
+        \"Start2\": 13,\n \
+        \"End2\": 15,\n \
+        \"Start3\": 18,\n \
+        \"End3\": 21,\n \
+        \"Start4\": 255,\n \
+        \"End4\": 255,\n \
+        \"Start5\": 255,\n \
+        \"End5\": 255\n \
+    },\n \
+    \"SegTime_shoulder\": {\n \
+        \"SegCont\": 4,\n \
+        \"Start1\": 7,\n \
+        \"End1\": 10,\n \
+        \"Start2\": 15,\n \
+        \"End2\": 16,\n \
+        \"Start3\": 17,\n \
+        \"End3\": 18,\n \
+        \"Start4\": 21,\n \
+        \"End4\": 23,\n \
+        \"Start5\": 255,\n \
+        \"End5\": 255\n \
+    },\n \
+    \"SegTime_off_peak\": {\n \
+        \"SegCont\": 2,\n \
+        \"Start1\": 23,\n \
+        \"End1\": 24,\n \
+        \"Start2\": 0,\n \
+        \"End2\": 7,\n \
+        \"Start3\": 255,\n \
+        \"End3\": 255,\n \
+        \"Start4\": 255,\n \
+        \"End4\": 255,\n \
+        \"Start5\": 255,\n \
+        \"End5\": 255\n \
+    }\n \
+}\n";
 
 const char *strFtpCfg =
 "{\n \
@@ -148,10 +163,25 @@ const char *strSysCfg =
     \"USE_RFID\": 1,\n \
     \"USE_GUI\":1\n \
 }\n";
-   
-        
-const char *strWhiteListCfg ="[]";
-const char *strBlackListCfg ="[]";
     
-const char *strOrderCfg ="[]";
-const char *strLogCfg ="[]";
+    
+//Adapter   1:以太网  2: 2G  3: 3G  4: 4G 5: WIFI
+//DHCP   1:使能  0:禁止
+const char *strNetCfg =
+"{\n \
+    \"Adapter\": 1,\n \
+    \"DHCP\": 1,\n \
+    \"HostName\":\"RGW1 Charger\",\n \
+    \"MAC\":\"00-AB-CD-EF-04-29\",\n \
+    \"IP\":\"192.168.153.105\",\n \
+    \"Mask\":\"192.168.30.1\",\n \
+    \"Gate\":\"255.255.255.0\",\n \
+    \"DNS1\":\"192.168.30.1\",\n \
+    \"DNS2\":\"0.0.0.0\"\n \
+}\n";
+        
+const char *strWhiteListCfg ="[]\n";
+const char *strBlackListCfg ="[]\n";
+    
+const char *strOrderCfg ="[]\n";
+const char *strLogCfg ="[]\n";
