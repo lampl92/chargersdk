6c83d1d9df07842871db4d7a0bc165476deac44f
rgw <rgw5267@gmail.com>
2018-06-05 13:03:13 +0800

    增加meter命令

--- src/cli/cli_main.c
+++ src/cli/cli_main.c
@@ -72,6 +72,7 @@ void cli_init(void)
     tinysh_add_command(&cli_networkinfo_cmd);
     tinysh_add_command(&cli_printlog_cmd);
     tinysh_add_command(&cli_setload_cmd);
+    tinysh_add_command(&cli_meter_cmd);
 #endif
     /************设备测试****************/
 //    tinysh_add_command(&cli_gprs_cmd);
--- src/cli/cli_main.h
+++ src/cli/cli_main.h
@@ -53,7 +53,7 @@ extern tinysh_cmd_t cli_printlog_cmd;
 extern tinysh_cmd_t cli_taskstack_cmd;
 extern tinysh_cmd_t cli_setload_cmd;
 extern tinysh_cmd_t cli_exit_cmd;
-
+extern tinysh_cmd_t cli_meter_cmd;
 
 
 #endif
