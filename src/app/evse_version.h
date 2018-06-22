313d58b481cfd9a10e492cfb27995d9991df163c
rgw <rgw5267@gmail.com>
2018-06-22 17:33:02 +0800

    Release:01.07.0622

--- src/app/evse_version.h
+++ src/app/evse_version.h
@@ -1,5 +1,5 @@
-#ifndef EVSE_VERSION_01_07_0621_H
-#define EVSE_VERSION_01_07_0621_H
+#ifndef EVSE_VERSION_01_07_0622_H
+#define EVSE_VERSION_01_07_0622_H
 
 //                   ↑   ↑  ↑
 //                   |   |  |
@@ -8,7 +8,7 @@
 
     #define EVSE_MAJOR          "01"              //主版本,两位, 对应生产用程序ABCD版本
     #define EVSE_MINOR          "07"              //副版本,两位, 对应PDM A1,A2版本
-    #define EVSE_PATCH          "0621"            //补丁版本,四位, 目前用作构建日期, 月日表示
+    #define EVSE_PATCH          "0622"            //补丁版本,四位, 目前用作构建日期, 月日表示
 	
 	
     #define EVSE_TARGET         "S1639"         //项目号,订单号,整改号
