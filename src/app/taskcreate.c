f213ef88dfbe14aff6be5629361c6de789f8715c
rgw <rgw5267@gmail.com>
2018-08-07 10:03:22 +0800

    降低cli优先级

--- src/app/taskcreate.c
+++ src/app/taskcreate.c
@@ -69,7 +69,7 @@
 #define defPRIORITY_TaskGUI                 4   //不能高,GUI任务时间太长,会影响硬件响应
 #define defPRIORITY_TaskGuidingLights       4
 #define defPRIORITY_TaskGUIBS               5
-#define defPRIORITY_TaskCLI                 16  //1. 原优先级2，修改16保证执行添加订单时订单存储被高优先级任务打断。2017年12月15日
+#define defPRIORITY_TaskCLI                 2  //1. 原优先级2，修改16保证执行添加订单时订单存储被高优先级任务打断。2017年12月15日
 
 //#define TCPIP_THREAD_PRIO         13 //defined in lwipopts.h
 // define PPP_THREAD_PRIO          14
