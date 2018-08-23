e685b11f1eaf24d5086360cdbc1ad19fb02929fe
rgw <rgw5267@gmail.com>
2018-08-23 14:48:35 +0800

    add cli gps

---
+++ src/cli/cli_gps.c
@@ -0,0 +1,19 @@
+﻿#include "tinysh.h"
+#include "minmea.h"
+
+void cli_gps_fnt(int argc, char **argv)
+{
+    minmea_main(argc, argv);
+}
+
+tinysh_cmd_t cli_gps_cmd =
+{
+    0,
+    "gps",
+    "gps",
+    0,
+    cli_gps_fnt,
+    "<cr>",
+    0,
+    0
+};
