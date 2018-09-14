ebaaaeb87adb414ac554d16a8360f0c081e5b3be
rgw <rgw5267@gmail.com>
2018-09-14 17:11:32 +0800

    Release：02.09.09141 修改超级密码规则

--- src/app/evse_version.h
+++ src/app/evse_version.h
@@ -8,7 +8,7 @@
 
     #define EVSE_MAJOR          "02"              //主版本,两位, 对应生产用程序ABCD版本
     #define EVSE_MINOR          "09"              //副版本,两位, 对应PDM A1,A2版本
-    #define EVSE_PATCH          "09140"           //补丁版本,四位, 目前用作构建日期, 月日表示,
+    #define EVSE_PATCH          "09141"           //补丁版本,四位, 目前用作构建日期, 月日表示,
 	
 	
     #define EVSE_TARGET         "S1639-1"         //项目号,订单号,整改号
