c59551e9258660b663aaa5a28f5c507f3a5a9c98
rgw <rgw5267@gmail.com>
2018-09-28 15:54:33 +0800

    Release：02.10.09281 电表串口复位 GPRS模块信号强度覆盖不全

--- src/app/evse_version.h
+++ src/app/evse_version.h
@@ -1,5 +1,5 @@
-#ifndef EVSE_VERSION_02_09_0914H
-#define EVSE_VERSION_02_09_0914H
+#ifndef EVSE_VERSION_02_10_0928H
+#define EVSE_VERSION_02_10_0928H
 
 //                   ↑   ↑  ↑
 //                   |   |  |
@@ -7,8 +7,8 @@
 
 
     #define EVSE_MAJOR          "02"              //主版本,两位, 对应生产用程序ABCD版本
-    #define EVSE_MINOR          "09"              //副版本,两位, 对应PDM A1,A2版本
-    #define EVSE_PATCH          "09141"           //补丁版本,四位, 目前用作构建日期, 月日表示,
+    #define EVSE_MINOR          "10"              //副版本,两位, 对应PDM A1,A2版本
+    #define EVSE_PATCH          "09281"           //补丁版本,四位, 目前用作构建日期, 月日表示,
 	
 	
     #define EVSE_TARGET         "S1639-1"         //项目号,订单号,整改号
