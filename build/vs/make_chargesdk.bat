577b42cf3d3faa21c6501e11ad13eb87dbb28bdd
rgw <rgw5267@gmail.com>
2018-10-12 20:37:50 +0800

    通过genflist生成flist。json

--- build/vs/make_chargesdk.bat
+++ build/vs/make_chargesdk.bat
@@ -1,5 +1,6 @@
 SET source=".\chargesdk\Release\chargesdk.bin"
 set crc32sum=..\..\tools\crc32sum.exe
+set genflist=..\..\tools\genflist\bin\genflist.exe
 set dd=%date:~8,2%
 set mm=%date:~5,2%
 set yy=%date:~0,4%
@@ -7,18 +8,19 @@ set Tss=%TIME:~6,2%
 set Tmm=%TIME:~3,2%
 set Thh=%TIME:~0,2%
 set Thh=%Thh: =0%
-set folder1="%yy%.%mm%.%dd%"
-set folder2="%Thh%.%Tmm%.%Tss%"
+set folder1=%yy%.%mm%.%dd%
+set folder2=%Thh%.%Tmm%.%Tss%
 set /p _ver=<"..\..\src\app\evse_version.h"
 set ver=%_ver:~21,10%
 echo %ver%
 set maj=%ver:~0,2%
 set min=%ver:~3,2%
 set patch=%ver:~6,4%
-set folder3="%maj%.%min%.%patch%"
+set folder3=%maj%.%min%.%patch%
 for /f "delims=" %%t in ('%crc32sum% -r %source%') do set _crc32=%%t
 set crc32=%_crc32:~34,8%
 md .\Release\chargesdk\%folder1%\%folder2%\%maj%.%min%.%patch%\upgrade
-copy /y ..\..\nand\template\flist.json .\Release\chargesdk\%folder1%\%folder2%\%folder3%
 copy /y %source% .\Release\chargesdk\%folder1%\%folder2%\%folder3%\upgrade\new_fw_%crc32%
 copy /y %source% .\Release\chargesdk.bin
+%genflist%  ..\..\..\build\vs\Release\chargesdk\%folder1%\%folder2%\%folder3%\upgrade\new_fw_%crc32%
+move /y ..\..\tools\genflist\bin\flist.json .\Release\chargesdk\%folder1%\%folder2%\%folder3%
