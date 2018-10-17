33ae3722192cf57cef46ed4de836b72d62076118
rgw <rgw5267@gmail.com>
2018-10-17 10:40:54 +0800

    增加关闭ftp命令，并重连服务器

--- src/cli/cli_net.c
+++ src/cli/cli_net.c
@@ -4,7 +4,14 @@
 void cli_ftpserver_fnt(int argc, char **argv)
 {
     extern void netChangeState(net_device_t *net_dev, net_state new_state);
-    netChangeState(net_dev, NET_STATE_FTPServer);
+    if (argc == 1)
+    {
+        netChangeState(net_dev, NET_STATE_FTPServer);
+    }
+    else if (argv[1][0] == '0')
+    {
+        netChangeState(net_dev, NET_STATE_DISCONNECT);
+    }
 }
 
 tinysh_cmd_t cli_ftpserver_cmd =
