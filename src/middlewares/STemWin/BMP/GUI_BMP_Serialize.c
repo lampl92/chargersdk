7c77530def623baab9e70334849d9f16b0f1b5bb
rgw <rgw5267@gmail.com>
2018-11-08 10:59:55 +0800

    代码格式化

--- src/middlewares/STemWin/BMP/GUI_BMP_Serialize.c
+++ src/middlewares/STemWin/BMP/GUI_BMP_Serialize.c
@@ -6,7 +6,7 @@
 *                       (c) Copyright 2002, Micrium Inc., Weston, FL
 *                       (c) Copyright 2002, SEGGER Microcontroller Systeme GmbH
 *
-*              µC/GUI is protected by international copyright laws. Knowledge of the
+*              Á§ê/GUI is protected by international copyright laws. Knowledge of the
 *              source code may not be used to write a similar product. This file may
 *              only be used in accordance with a license and should not be redistributed
 *              in any way. We appreciate your understanding and fairness.
@@ -26,7 +26,7 @@ Purpose     : Implementation of GUI_BMP_Serialize
 **********************************************************************
 */
 
-#define DIB_HEADER_MARKER ((U16) ('M' << 8) | 'B') /* DIB Header Marker - used in writing DIBs to files */
+#define DIB_HEADER_MARKER ((U16)('M' << 8) | 'B') /* DIB Header Marker - used in writing DIBs to files */
 
 #define SIZEOF_BITMAPFILEHEADER 14
 #define SIZEOF_BITMAPINFOHEADER 40
@@ -41,7 +41,8 @@ Purpose     : Implementation of GUI_BMP_Serialize
 *
 *       _SendU16
 */
-static void _SendU16(GUI_CALLBACK_VOID_U8_P * pfSerialize, U16 Data, void * p) {
+static void _SendU16(GUI_CALLBACK_VOID_U8_P *pfSerialize, U16 Data, void *p)
+{
     (*pfSerialize)((U8)Data, p);
     (*pfSerialize)(Data >> 8, p);
 }
@@ -50,7 +51,8 @@ static void _SendU16(GUI_CALLBACK_VOID_U8_P * pfSerialize, U16 Data, void * p) {
 *
 *       _SendU32
 */
-static void _SendU32(GUI_CALLBACK_VOID_U8_P * pfSerialize, U32 Data, void * p) {
+static void _SendU32(GUI_CALLBACK_VOID_U8_P *pfSerialize, U32 Data, void *p)
+{
     _SendU16(pfSerialize, (U16)Data, p);
     _SendU16(pfSerialize, Data >> 16, p);
 }
@@ -65,64 +67,67 @@ static void _SendU32(GUI_CALLBACK_VOID_U8_P * pfSerialize, U32 Data, void * p) {
 *
 *       GUI_BMP_SerializeEx
 */
-void myGUI_BMP_SerializeEx(GUI_CALLBACK_VOID_U8_P * pfSerialize, int x0, int y0, int xSize, int ySize, void * p) {
+void myGUI_BMP_SerializeEx(GUI_CALLBACK_VOID_U8_P *pfSerialize, int x0, int y0, int xSize, int ySize, void *p)
+{
     int x, y, i, BPP, BytesPerLine, NumColors, Padding;
     U32 BitmapOffset, FileSize;
     GUI_LOCK();
-    BPP          = LCD_GetBitsPerPixel();
+    BPP = LCD_GetBitsPerPixel();
     BytesPerLine = ((BPP > 8) ? (2 * xSize + 2) : (xSize + 3)) & ~3;
-    NumColors    = (BPP > 8) ? 0 : (1 << BPP);
-    Padding      = BytesPerLine - ((BPP > 8) ? (2 * xSize) : xSize);
+    NumColors = (BPP > 8) ? 0 : (1 << BPP);
+    Padding = BytesPerLine - ((BPP > 8) ? (2 * xSize) : xSize);
     /* Write BITMAPFILEHEADER */
-    BitmapOffset   = SIZEOF_BITMAPFILEHEADER
-                   + SIZEOF_BITMAPINFOHEADER
-                   + NumColors * 4;
-    FileSize       = BitmapOffset
-                   + ySize * BytesPerLine;
+    BitmapOffset = SIZEOF_BITMAPFILEHEADER + SIZEOF_BITMAPINFOHEADER + NumColors * 4;
+    FileSize = BitmapOffset + ySize * BytesPerLine;
     _SendU16(pfSerialize, DIB_HEADER_MARKER, p); /* bfType */
-    _SendU32(pfSerialize, FileSize, p); /* bfSize */
-    _SendU32(pfSerialize, 0, p); /* bfReserved1, bfReserved2 */
-    _SendU32(pfSerialize, BitmapOffset, p); /* bfOffBits */
+    _SendU32(pfSerialize, FileSize, p);          /* bfSize */
+    _SendU32(pfSerialize, 0, p);                 /* bfReserved1, bfReserved2 */
+    _SendU32(pfSerialize, BitmapOffset, p);      /* bfOffBits */
     /* Write BITMAPINFOHEADER */
     _SendU32(pfSerialize, SIZEOF_BITMAPINFOHEADER, p); /* biSize */
-    _SendU32(pfSerialize, xSize, p); /* biWidth */
-    _SendU32(pfSerialize, ySize, p); /* biHeighth */
-    _SendU16(pfSerialize, 1, p); /* biPlanes */
-    _SendU16(pfSerialize, (BPP  <= 8) ? 8 : 16, p); /* biBitCount */
-    _SendU32(pfSerialize, 0, p); /* biCompression */
-    _SendU32(pfSerialize, 0, p); /* biSizeImage */
-    _SendU32(pfSerialize, 0, p); /* biXPelsPerMeter */
-    _SendU32(pfSerialize, 0, p); /* biYPelsPerMeter */
-    _SendU32(pfSerialize, NumColors, p); /* biClrUsed */
-    _SendU32(pfSerialize, 0, p); /* biClrImportant */
+    _SendU32(pfSerialize, xSize, p);                   /* biWidth */
+    _SendU32(pfSerialize, ySize, p);                   /* biHeighth */
+    _SendU16(pfSerialize, 1, p);                       /* biPlanes */
+    _SendU16(pfSerialize, (BPP <= 8) ? 8 : 16, p);     /* biBitCount */
+    _SendU32(pfSerialize, 0, p);                       /* biCompression */
+    _SendU32(pfSerialize, 0, p);                       /* biSizeImage */
+    _SendU32(pfSerialize, 0, p);                       /* biXPelsPerMeter */
+    _SendU32(pfSerialize, 0, p);                       /* biYPelsPerMeter */
+    _SendU32(pfSerialize, NumColors, p);               /* biClrUsed */
+    _SendU32(pfSerialize, 0, p);                       /* biClrImportant */
     /* Write palette */
-    for (i = 0; i < NumColors; i++) {
+    for (i = 0; i < NumColors; i++)
+    {
         U32 Color;
         Color = GUI_Index2Color(i);
         Color = ((Color >> 16) & 255) | (Color & 0xff00) | ((Color & 0xff) << 16);
         _SendU32(pfSerialize, Color, p);
     }
     /* Write pixels */
-    for (y = ySize - 1; y >= 0; y--) {
-        for (x = 0; x < xSize; x++) {
-            if (BPP <= 8) {
+    for (y = ySize - 1; y >= 0; y--)
+    {
+        for (x = 0; x < xSize; x++)
+        {
+            if (BPP <= 8)
+            {
                 pfSerialize(LCD_GetPixelIndex(x0 + x, y0 + y), p);
             }
-            else {
+            else
+            {
                 U16 Color16 = 0;
                 U32 Color = LCD_GetPixelColor(x0 + x, y0 + y);
                 int b = ((Color >> 16) * 31 + 127) / 255;
                 int g = (((Color >> 8) & 255) * 31 + 127) / 255;
                 int r = ((Color & 255) * 31 + 127) / 255;
-          
-                //≥¢ ‘∫Û16Œª
-                //µÕ◊÷Ω⁄µƒ«∞5Œª”√¿¥±Ì æB(BLUE)
-                //µÕ◊÷Ω⁄µƒ∫Û»˝Œª+∏ﬂ◊÷Ω⁄µƒ«∞»˝Œª”√¿¥±Ì æG(Green)
-                //∏ﬂ◊÷Ω⁄µƒ∫Û5Œª”√¿¥±Ì æR(RED)
+
+                //Â∞ùËØïÂêé16‰Ωç
+                //‰ΩéÂ≠óËäÇÁöÑÂâç5‰ΩçÁî®Êù•Ë°®Á§∫B(BLUE)
+                //‰ΩéÂ≠óËäÇÁöÑÂêé‰∏â‰Ωç+È´òÂ≠óËäÇÁöÑÂâç‰∏â‰ΩçÁî®Êù•Ë°®Á§∫G(Green)
+                //È´òÂ≠óËäÇÁöÑÂêé5‰ΩçÁî®Êù•Ë°®Á§∫R(RED)
                 //         int b = Color & 0xF8;
                 //          int g = (Color & 0x07) | ((Color & 0xE000)>>10);
                 //          int r = (Color & 0x1F00) >> 8;
-          
+
                 //          void CPixelRGB565::SideEffectColorChanged(void)
                 //          {
                 //              CBasePixel::SideEffectColorChanged();
@@ -143,18 +148,17 @@ void myGUI_BMP_SerializeEx(GUI_CALLBACK_VOID_U8_P * pfSerialize, int x0, int y0,
                 //              wMask     = wMask >> 8;
                 //              m_Mask[1] = wMask & 0xff;
                 //          }
-                          //0xF800, 0x07E0, 0x001F
+                //0xF800, 0x07E0, 0x001F
 
-          
-          
-    Color16 = (r << 10) | (g << 5) | b;       /* 16 bpp Bitmaps in windows are 555: rrrrrgggggbbbbb */
-//Color16 = ((Color >> 19 & 31) << 11) | ((Color >> 10 & 63) << 5) | (Color >> 3 & 31);
-_SendU16(pfSerialize, Color16, p);
+                Color16 = (r << 10) | (g << 5) | b; /* 16 bpp Bitmaps in windows are 555: rrrrrgggggbbbbb */
+                //Color16 = ((Color >> 19 & 31) << 11) | ((Color >> 10 & 63) << 5) | (Color >> 3 & 31);
+                _SendU16(pfSerialize, Color16, p);
                 //_SendU32(pfSerialize, Color, p);
             }
         }
         /* Align pointer to next U32 */
-        for (i = 0; i < Padding; i++) {
+        for (i = 0; i < Padding; i++)
+        {
             pfSerialize(0, p);
         }
     }
@@ -165,7 +169,8 @@ _SendU16(pfSerialize, Color16, p);
 *
 *       GUI_BMP_Serialize
 */
-void myGUI_BMP_Serialize(GUI_CALLBACK_VOID_U8_P * pfSerialize, void * p) {
+void myGUI_BMP_Serialize(GUI_CALLBACK_VOID_U8_P *pfSerialize, void *p)
+{
     int x = LCD_GetXSize();
     int y = LCD_GetYSize();
     myGUI_BMP_SerializeEx(pfSerialize, 0, 0, x, y, p);
@@ -173,4 +178,3 @@ void myGUI_BMP_Serialize(GUI_CALLBACK_VOID_U8_P * pfSerialize, void * p) {
 }
 
 /*************************** End of file ****************************/
-	 	 			 		    	 				 	  			   	 	 	 	 	 	  	  	      	   		 	 	 		  		  	 		 	  	  			     			       	   	 			  		    	 	     	 				  	 					 	 			   	  	  			 				 		 	 	 			     			 
