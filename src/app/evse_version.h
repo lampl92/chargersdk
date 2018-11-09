89e0ecc1bc3aabb2622019c5a4a236aba98c3cb9
rgw <rgw5267@gmail.com>
2018-11-09 17:36:57 +0800

    Release:02.11.11090 修改modem连接时间

--- src/app/evse_version.h
+++ src/app/evse_version.h
@@ -1,5 +1,5 @@
-#ifndef EVSE_VERSION_02_11_1029H
-#define EVSE_VERSION_02_11_1029H
+#ifndef EVSE_VERSION_02_11_1109H
+#define EVSE_VERSION_02_11_1109H
 
 //                   ↑   ↑  ↑
 //                   |   |  |
@@ -8,7 +8,7 @@
 
     #define EVSE_MAJOR          "02"              //主版本,两位, 对应生产用程序ABCD版本
     #define EVSE_MINOR          "11"              //副版本,两位, 对应PDM A1,A2版本
-    #define EVSE_PATCH          "10290"           //补丁版本,四位, 目前用作构建日期, 月日表示,
+    #define EVSE_PATCH          "11090"           //补丁版本,四位, 目前用作构建日期, 月日表示,
 
 
     #define EVSE_TARGET         "S1639-1"         //项目号,订单号,整改号
